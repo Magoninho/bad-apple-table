4a9181a6e7f488da62fc552b5713d3312cc3a9ed
Magoninho <magoninhogamer@gmail.com>
2022-06-16 18:36:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bad Apple, but in HTML table
+An implementation of bad apple, but using \<table\>
+
+![demo](demo.gif)
---
+++ script.js
@@ -0,0 +1,54 @@
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+let myvideo = document.getElementById("video");
+canvas.width = myvideo.width;
+canvas.height = myvideo.height;
+
+let sample_size = 5; // "donwscale factor"
+
+function createTable(width, height) {
+	var table = document.createElement('table');
+	var tableBody = document.createElement('tbody');
+
+	for (let y = 0; y < height; y++) {
+		let row = table.insertRow(y);
+		for (let x = 0; x < width; x++) {
+			row.insertCell(x);
+		}
+	}
+
+	table.appendChild(tableBody);
+	document.body.appendChild(table);
+	return table;
+}
+
+let table = createTable(canvas.width / sample_size, canvas.height / sample_size);
+
+
+// fills a cell from a table
+function fillCell(table, x, y, color) {
+	table.rows[y].cells[x].style.backgroundColor = color;
+}
+
+
+function getPixelRGB(ctx, x, y) {
+	const pixel_data = ctx.getImageData(x, y, 1, 1).data;
+	return `rgb(${pixel_data[0]}, ${pixel_data[1]}, ${pixel_data[2]})`;
+}
+
+myvideo.addEventListener("play", function () {
+	let frame = myvideo;
+	function loop() {
+		ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+		for (let y = 0; y < myvideo.height; y += sample_size) {
+			for (let x = 0; x < myvideo.width; x += sample_size) {
+				// dividing by sample size because the table size is already downscaled
+				fillCell(table, x / sample_size, y / sample_size, getPixelRGB(ctx, x, y));
+
+			}
+		}
+		requestAnimationFrame(loop)
+	}
+	requestAnimationFrame(loop);
+})
+
