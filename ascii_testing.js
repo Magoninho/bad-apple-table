9e6502da852ba84821a6adb242d8e78feda04b21
Magoninho <magoninhogamer@gmail.com>
2022-06-17 15:48:35 -0300

    custom scale

--- ascii_testing.js
+++ ascii_testing.js
@@ -3,7 +3,8 @@ let ctx = canvas.getContext("2d");
 let myvideo = document.getElementById("video");
 canvas.width = myvideo.width;
 canvas.height = myvideo.height;
-let sample_size = parseInt(prompt("pixel scale:")); // "donwscale factor"
+
+let sample_size = 5; // "donwscale factor"
 
 function createTable(width, height) {
 	var table = document.createElement('table');
@@ -17,22 +18,31 @@ function createTable(width, height) {
 	}
 
 	table.appendChild(tableBody);
+	document.body.appendChild(table);
 	return table;
 }
-
 
 let table = createTable(canvas.width / sample_size, canvas.height / sample_size);
 
 
 // fills a cell from a table
 function fillCell(table, x, y, color) {
-	table.rows[y].cells[x].style.backgroundColor = color;
+	// table.rows[y].cells[x].style.backgroundColor = color;
+	table.rows[y].cells[x].innerHTML = color;
 }
 
 
 function getPixelRGB(ctx, x, y) {
 	const pixel_data = ctx.getImageData(x, y, 1, 1).data;
-	return `rgb(${pixel_data[0]}, ${pixel_data[1]}, ${pixel_data[2]})`;
+	const ascii_map = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+	const grayscale = (pixel_data[0] + pixel_data[1] + pixel_data[2]) / 3;
+
+	let index = Math.floor((grayscale * ascii_map.length) / 255);
+
+	// return `rgb(${pixel_data[0]}, ${pixel_data[1]}, ${pixel_data[2]})`;
+	// return `rgb(${grayscale}, ${grayscale}, ${grayscale})`;
+	return `<span style="color: rgb(${pixel_data[0]}, ${pixel_data[1]}, ${pixel_data[2]})">${ascii_map.charAt(index)}</span>`;
 }
 
 myvideo.addEventListener("play", function () {
@@ -46,9 +56,8 @@ myvideo.addEventListener("play", function () {
 
 			}
 		}
-		requestAnimationFrame(loop);
+		requestAnimationFrame(loop)
 	}
 	requestAnimationFrame(loop);
-});
+})
 
-document.body.appendChild(table);
